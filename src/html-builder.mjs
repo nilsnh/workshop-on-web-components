46d9145650f48b87b2ca1c4eccfcc535612446b1
Nils Norman Haukås <mail@nilsnh.no>
2022-03-16 07:17:06 +0100

    :pencil: comment text

--- src/html-builder.mjs
+++ src/html-builder.mjs
@@ -1,3 +1,24 @@
+/**
+ * This 'cr' function is a tiny abstraction on the document.createElement()
+ * function.
+ *
+ * Examples:
+ *
+ * First argument is the name of the tag you want to create.
+ * const anchorTag = cr('a')
+ *
+ * Second and third argument can be used to provide attributes and inner-text.
+ * const anchorTag = cr('a', {id: 'some-id', href: '/'}, 'this is a link')
+ *
+ * You can create nested trees like so:
+ * cr('div',
+ *  cr('p', 'My first paragraph'),
+ *  cr('p', 'then there's the second paragraph')
+ * )
+ *
+ * @param  {...any} args One or more arguments.
+ * @returns One or more HTML elements.
+ */
 export const cr = (...args) => {
   const [tagname, ...restOfArgs] = args
   const newElement = document.createElement(tagname)
